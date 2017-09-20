3b7bc5260f7604bb86f252d96630759976f62b55
billyjack1988 <billyjacktattoos@gmail.com>
2017-09-20 08:06:41 -0500

    updated alot didint have internet last night

--- app.rb
+++ app.rb
@@ -20,8 +20,63 @@ get '/resultspage' do
 	erb :results, locals:{answer:answer,phone_book:phone_book}
 end
 
+post "/search" do
+	lname = params[:lname]
+	phone = params[:phone]
+	
+	if phone == "" and lname == ""
+		session[:search_answer] = "Need search term"
+	elsif phone == ""
+		session[:search_answer] = search_data_lname(lname)
+	else
+		session[:search_answer] = search_data_phone(phone)
+	end
+	redirect "/search_answer?"
+end
 
+get "/search_answer" do
 
+erb :seachandupdate, locals:{search_answer:session[:search_answer]}
+end
 
+post "/update" do
+	session[:info] = params[:info]
+	choose = params[:choose]
+	if session[:info] == nil
+		answer = "Didn't Change"
+		redirect "/answer_page?answer=" + answer
+	else
+		if choose == "update"
+			redirect "/update_answer?"
+		else
+			redirect "/delete?"
+		end
+	end
 
+end
 
+get "/update_answer" do
+erb :update, locals:{info:session[:info]}
+end
+
+post '/updated' do
+	answer = "Info Updated"
+  updated_info = params[:info]
+	updated_slice = updated_info.each_slice(7).to_a
+  info = session[:info]
+  old_phone = []
+  
+  info.each do |row|
+  	split = row.split(',')
+  	old_phone << split[-1]
+  end
+   update_table(updated_slice,old_phone)
+   redirect "/resultspage?answer=" + answer
+end
+
+get "/delete" do
+	answer = "Info DELETED"
+	deleting_info = session[:info]
+	delete_from_table(deleting_info)
+	redirect "/resultspage?answer=" + answer
+end
--- my_func.rb
+++ my_func.rb
@@ -41,3 +41,81 @@ db = PG::Connection.new(db_params)
     end
    data
 end
+
+def search_data_phone(info)
+	db_params = {
+	host: ENV['host'],
+	port: ENV['port'],
+	dbname: ENV['dbname'],
+	user: ENV['user'],
+	password: ENV['password']
+}
+	db = PG::Connection.new(db_params)
+	check = db.exec("SELECT * FROM phonenum_table WHERE phone = '#{info}'")
+	
+	if check.num_tuples.zero? == false
+			search_answer = check.values
+	else
+		search_answer = "isnt in phone book"
+	end
+
+	end
+
+def search_data_lname(info)
+	db_params = {
+	host: ENV['host'],
+	port: ENV['port'],
+	dbname: ENV['dbname'],
+	user: ENV['user'],
+	password: ENV['password']
+}
+db = PG::Connection.new(db_params)
+check = db.exec("SELECT * FROM phonenum_table WHERE last_name = '#{info}'")
+woo = check.num_tuples
+if check.num_tuples.zero? == false
+	search_answer = check.values
+else
+	search_answer = "isnt in phone book"
+end
+end
+# get_info_database()
+
+
+def update_table(info_new,old_phone)
+		db_params = {
+	host: ENV['host'],
+	port: ENV['port'],
+	dbname: ENV['dbname'],
+	user: ENV['user'],
+	password: ENV['password']
+}
+db = PG::Connection.new(db_params)
+	counter = 0
+	info_new.each do |arr|
+		db.exec("UPDATE phonenum_table SET first_name = '#{arr[0]}' ,last_name = '#{arr[1]}',address = '#{arr[2]}',city = '#{arr[3]}',state = '#{arr[4]}',zip = '#{arr[5]}',phone = '#{arr[6]}'WHERE phone = '#{old_phone[counter]}'")
+		counter =+ 1
+	end	
+end
+
+def delete_from_table(delete_info)
+	db_params = {
+	host: ENV['host'],
+	port: ENV['port'],
+	dbname: ENV['dbname'],
+	user: ENV['user'],
+	password: ENV['password']
+}
+db = PG::Connection.new(db_params)
+	arr = []
+	delete_info.each do |row|
+		woo = row.split(',')
+		arr << woo
+	end
+	# delete_split = delete_info.split(',')
+	# delete_slice = delete_slice.slice(7).to_a
+	arr.each do |row|
+		delete_num = row[-1]
+		db.exec("DELETE FROM phonenum_table WHERE phone = '#{delete_num}'")
+	end
+
+end
